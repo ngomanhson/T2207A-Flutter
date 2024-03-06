e131cc1d0a81411f82eb9d0e8f32dcc630789aa2
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-03-06 14:41:29 +0700

    carousel

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mb_app/screens/home/ui/home_carousel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        Text("Home screen"),
+        HomeCarousel(),
         Text("Second section"),
         Text("Second section"),
       ],
